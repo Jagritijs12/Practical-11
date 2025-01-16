86f10f1d2853ddc14135ab9b88d48fa8cfe03d5b
Jagriti <jagriti.singh22@st.niituniversity.in>
2025-01-16 21:28:14 +0530

    modified files

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+// Function to clear the calculator screen
 function clearScreen() {
-    document.getElementById("result").value = "";
-  }
-  
-  function appendValue(value) {
-    document.getElementById("result").value += value;
-  }
-  
-  function calculate() {
-    try {
-      const result = eval(document.getElementById("result").value);
-      if (isNaN(result) || !isFinite(result)) {
-        throw new Error("Invalid calculation");
-      }
-      document.getElementById("result").value = result;
-    } catch (e) {
-      alert("Invalid Input");
-      clearScreen();
+  document.getElementById("result").value = "";
+}
+
+// Function to append a value to the calculator screen
+function appendValue(value) {
+  document.getElementById("result").value += value;
+}
+
+// Function to calculate the result of the entered expression
+function calculate() {
+  try {
+    const result = eval(document.getElementById("result").value);
+    if (isNaN(result) || !isFinite(result)) {
+      throw new Error("Invalid calculation");
     }
+    document.getElementById("result").value = result;
+  } catch (e) {
+    alert("Invalid Input");
+    clearScreen();
   }
-  
+}
+
